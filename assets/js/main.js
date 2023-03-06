44d328b446ee09e12e156ed871da04f47d916c21
mutugi-c <mutugichris@gmail.com>
2023-03-06 22:28:38 +0100

    Add query selectors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,3 @@
-
+const hamburgerButton = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('#mobile-menu');
+const closeButton = document.querySelector('.close-icon');
