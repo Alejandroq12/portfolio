d6bae7380eb3a306fdb5ae4d29d7224e878a9788
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-07 16:15:40 -0600

    Generate objects with JavaScript

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -49,4 +49,27 @@ const projects = [
   },
 ];
 
-export default projects;
+const projectsContainer = document.querySelector('.works-container');
+
+projects.forEach((project) => {
+  // Create a new card element
+  const card = document.createElement('div');
+  card.classList.add('work-card');
+  card.setAttribute('tabindex', '0');
+  card.setAttribute('aria-label', project.name);
+
+  // Fill in the card's HTML with the project's information
+  card.innerHTML = `
+    <h4 class="work-cards-title" tabindex="0">${project.name}</h4>
+    <p class="work-cards-description" tabindex="0">${project.description}</p>
+    <ul>
+      ${project.technologies.map((tech) => `<li tabindex="0">${tech}</li>`).join('')}
+    </ul>
+    <button class="button-card" type="submit" name="see project button" aria-label="see project button" tabindex="0">
+      See Project
+    </button>
+  `;
+
+  // Append the card to the container
+  projectsContainer.appendChild(card);
+});
