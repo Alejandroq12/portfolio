35ce3a644b9b3a68b133a803931bd6f69a2ddb94
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-07 15:29:57 -0600

    Add JavaScript objects

---
+++ assets/js/projects.js
@@ -0,0 +1,52 @@
+const projects = [
+  {
+    name: 'Professional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    image: 'img/project1.jpg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com',
+    sourceLink: 'https://github.com/example',
+  },
+  {
+    name: 'Another Project',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'img/project2.jpg',
+    technologies: ['JavaScript', 'CSS'],
+    liveLink: 'https://example.com',
+    sourceLink: 'https://github.com/example',
+  },
+  {
+    name: 'Another Project',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'img/project3.jpg',
+    technologies: ['JavaScript', 'CSS'],
+    liveLink: 'https://example.com',
+    sourceLink: 'https://github.com/example',
+  },
+  {
+    name: 'Another Project',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'img/project4.jpg',
+    technologies: ['JavaScript', 'CSS'],
+    liveLink: 'https://example.com',
+    sourceLink: 'https://github.com/example',
+  },
+  {
+    name: 'Another Project5',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'img/project2.jpg',
+    technologies: ['JavaScript', 'CSS'],
+    liveLink: 'https://example.com',
+    sourceLink: 'https://github.com/example',
+  },
+  {
+    name: 'Another Project6',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'img/project2.jpg',
+    technologies: ['JavaScript', 'CSS'],
+    liveLink: 'https://example.com',
+    sourceLink: 'https://github.com/example',
+  },
+];
+
+export default projects;
