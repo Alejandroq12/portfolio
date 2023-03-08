94f1681c7696f0195f277f8f53e96a0ce01e97f4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-08 16:25:37 -0600

    Fix path to image

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,17 @@
 import projects from './projects.js';
 
-
 const popupContainer = document.querySelector('#popup-container');
 const bodyEl = document.querySelector('body');
 const popupEl = document.querySelector('.popup');
-
 
 function generatePopup(project) {
   const popup = document.createElement('div');
   popup.classList.add('popup');
 
-
   const closeButton = document.createElement('button');
   closeButton.classList.add('popup-close');
 
-
   closeButton.innerHTML = '<img src="./assets/images/icons/x2.png" alt="Close">';
-
 
   closeButton.addEventListener('click', () => {
     popup.remove();
@@ -24,22 +19,18 @@ function generatePopup(project) {
     popupEl.style.display = 'none';
   });
 
-
   const header = document.createElement('h2');
   header.classList.add('popup-header');
   header.textContent = project.name;
-
 
   const image = document.createElement('img');
   image.classList.add('popup-image');
   image.src = project.image;
   image.alt = project.name;
 
-
   const description = document.createElement('p');
   description.classList.add('popup-description');
   description.textContent = project.description;
-
 
   const technologiesList = document.createElement('ul');
   technologiesList.classList.add('popup-technologies');
@@ -49,28 +40,23 @@ function generatePopup(project) {
     technologiesList.appendChild(technology);
   });
 
-
   const buttonsContainer = document.createElement('div');
   buttonsContainer.classList.add('popup-buttons');
-
 
   const liveLinkButton = document.createElement('a');
   liveLinkButton.classList.add('button', 'popup-button');
   liveLinkButton.href = project.liveLink;
   liveLinkButton.textContent = 'See Live';
 
-
   const liveLinkLogo = document.createElement('img');
   liveLinkLogo.src = 'assets/images/icons/live3.png';
   liveLinkLogo.alt = 'Live Link Logo';
   liveLinkLogo.classList.add('popup-link-logo');
 
-
   const sourceLinkButton = document.createElement('a');
   sourceLinkButton.classList.add('button', 'popup-button');
   sourceLinkButton.href = project.sourceLink;
   sourceLinkButton.textContent = 'See Source';
-
 
   const sourceLinkLogo = document.createElement('img');
   sourceLinkLogo.src = 'assets/images/icons/github.png';
@@ -78,14 +64,11 @@ function generatePopup(project) {
   sourceLinkLogo.classList.add('popup-link-logo');
   bodyEl.classList.add('popup-open');
 
-
   liveLinkButton.appendChild(liveLinkLogo);
   sourceLinkButton.appendChild(sourceLinkLogo);
 
-
   buttonsContainer.appendChild(liveLinkButton);
   buttonsContainer.appendChild(sourceLinkButton);
-
 
   popup.appendChild(closeButton);
   popup.appendChild(header);
@@ -93,7 +76,6 @@ function generatePopup(project) {
   popup.appendChild(description);
   popup.appendChild(technologiesList);
   popup.appendChild(buttonsContainer);
-
 
   popupContainer.appendChild(popup);
 }
@@ -105,14 +87,11 @@ seeProjectButtons.forEach((button, index) => {
   });
 });
 
-
 const navToggle = document.querySelector('.nav-toggle');
-
 
 navToggle.addEventListener('click', () => {
   popupContainer.classList.toggle('hidden');
 });
-
 
 if (window.innerWidth < 768) {
   popupContainer.classList.add('hidden');
