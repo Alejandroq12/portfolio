bfba6bba01ff360169441f6aa09212699605c541
mutugi-c <mutugichris@gmail.com>
2023-03-09 16:17:43 +0100

    Display error message for invalid email

--- js/validation-form.js
+++ js/validation-form.js
@@ -9,5 +9,15 @@ form.addEventListener('submit', (event) => {
   // check if the email is in lowercase
   if (emailValue !== emailValue.toLowerCase()) {
     event.preventDefault();
+
+    
+    // display an error message near the submit button
+    const submitButton = document.getElementById('contact-form-button');
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = 'Please enter a lowercase email address.';
+    errorMessage.style.color = 'red';
+    const errorContainer = document.createElement('div');
+    errorContainer.appendChild(errorMessage);
+    form.insertBefore(errorContainer, submitButton);
   }
 });
