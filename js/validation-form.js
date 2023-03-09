d4f75eb979cf8684e2f3d3743d2e6b90d3ef483f
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-09 09:51:08 -0600

    Check that the code works correctly

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,16 +1,13 @@
 const form = document.getElementById('form');
 
-
 form.addEventListener('submit', (event) => {
   const emailInput = document.getElementById('email');
   const emailValue = emailInput.value;
-
 
   // check if the email is in lowercase
   if (emailValue !== emailValue.toLowerCase()) {
     event.preventDefault();
 
-    
     // display an error message near the submit button
     const submitButton = document.getElementById('contact-form-button');
     const errorMessage = document.createElement('p');
