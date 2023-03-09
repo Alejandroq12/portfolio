8386879065f7d2617ee2c54cc99497c106b16406
mutugi-c <mutugichris@gmail.com>
2023-03-09 16:15:38 +0100

    Check email case before submission

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,1 +1,13 @@
+const form = document.getElementById('form');
 
+
+form.addEventListener('submit', (event) => {
+  const emailInput = document.getElementById('email');
+  const emailValue = emailInput.value;
+
+
+  // check if the email is in lowercase
+  if (emailValue !== emailValue.toLowerCase()) {
+    event.preventDefault();
+  }
+});
