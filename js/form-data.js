c98538494f198e3501d435639b9fa685e07fad10
mutugi-c <mutugichris@gmail.com>
2023-03-09 18:27:04 +0100

    Update local storage when form data changes

--- js/form-data.js
+++ js/form-data.js
@@ -6,3 +6,19 @@ const messageInput = document.getElementById('message');
 const savedData = localStorage.getItem('form-data');
 const formData = savedData ? JSON.parse(savedData) : {};
 
+// Update the local storage object when the input fields change
+nameInput.addEventListener('input', () => {
+    formData.name = nameInput.value;
+    localStorage.setItem('form-data', JSON.stringify(formData));
+  });
+  
+  emailInput.addEventListener('input', () => {
+    formData.email = emailInput.value;
+    localStorage.setItem('form-data', JSON.stringify(formData));
+  });
+  
+  messageInput.addEventListener('input', () => {
+    formData.message = messageInput.value;
+    localStorage.setItem('form-data', JSON.stringify(formData));
+  });
+  
