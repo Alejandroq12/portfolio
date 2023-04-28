491515990cbb9e5d5e859dcd6ca4dc13771a1a17
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-27 20:53:19 -0600

    Improve from validation

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,20 +1,57 @@
 const form = document.getElementById('form');
 
-form.addEventListener('submit', (event) => {
+const debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+};
+
+function escapeHTML(unsafe) {
+  return unsafe.replace(/[&<"']/g, (m) => ({
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;',
+  })[m]);
+}
+
+function createCustomValidity(input, message) {
+  const feedback = document.createElement('div');
+  feedback.classList.add('invalid-feedback');
+  feedback.textContent = message;
+
+  input.setCustomValidity(message);
+  input.insertAdjacentElement('afterend', feedback);
+
+  input.addEventListener('input', () => {
+    input.setCustomValidity('');
+    feedback.remove();
+  });
+}
+
+form.addEventListener('submit', debounce((event) => {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
-  const emailValue = emailInput.value;
+  const emailValue = escapeHTML(emailInput.value); // Escape HTML characters
 
-  // check if the email is in lowercase
-  if (emailValue !== emailValue.toLowerCase()) {
-    event.preventDefault();
+  // Use regex pattern for email validation
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
-    // display an error message near the submit button
-    const submitButton = document.getElementById('contact-form-button');
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = 'Please enter a lowercase email address.';
-    errorMessage.style.color = 'red';
-    const errorContainer = document.createElement('div');
-    errorContainer.appendChild(errorMessage);
-    form.insertBefore(errorContainer, submitButton);
+  if (!nameInput.validity.valid) {
+    createCustomValidity(nameInput, nameInput.title);
   }
-});
+
+  if (!emailRegex.test(emailValue)) {
+    createCustomValidity(emailInput, emailInput.title);
+  }
+
+  if (form.checkValidity()) {
+    // Process the form submission.
+  }
+}, 500));
